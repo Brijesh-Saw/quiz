e8eb7adbee6cce735c6a04ff55ed9891c28d9545
Brijesh-Saw <sawbrijesh7@gmail.com>
2020-12-25 13:48:17 +0530

    initial commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+import 'QuizPage.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> names = [
+    "Python",
+    "Java",
+    "JavaScript",
+    "C++",
+    "Linux",
+  ];
+
+  List<String> images = [
+    "images/py.png",
+    "images/java.png",
+    "images/js.png",
+    "images/cpp.png",
+    "images/linux.png",
+  ];
+
+  List<String> des = [
+    "Python is one of the most popular and fastest programming language since half a decade.\nIf You think you have learnt it.. \nJust test yourself !!",
+    "Java has always been one of the best choices for Enterprise World. If you think you have learn the Language...\nJust Test Yourself !!",
+    "Javascript is one of the most Popular programming language supporting the Web.\nIt has a wide range of Libraries making it Very Powerful !",
+    "C++, being a statically typed programming language is very powerful and Fast.\nit's DMA feature makes it more useful. !",
+    "Linux is a OPEN SOURCE Operating System which powers many Servers and Workstation.\nIt is also a top Priority in Developement Work !",
+  ];
+
+  Widget customCard(String name, String image, String description) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 30.0,
+      ),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => loadjson(name),
+              ));
+        },
+        child: Material(
+          color: Colors.blue,
+          elevation: 10.0,
+          borderRadius: BorderRadius.circular(25.0),
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(75.0),
+                    child: Container(
+                      height: 150.0,
+                      width: 150.0,
+                      child: ClipOval(
+                        child: Image(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            image,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontFamily: "Quando",
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    description,
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontFamily: "Alike"),
+                    maxLines: 5,
+                    textAlign: TextAlign.justify,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My Quiz",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          customCard(names[0], images[0], des[0]),
+          customCard(names[1], images[1], des[1]),
+          customCard(names[2], images[2], des[2]),
+          customCard(names[3], images[3], des[3]),
+          customCard(names[4], images[4], des[4]),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Quiz',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'My Quiz'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.tealAccent,
+      body: Center(
+        child: Text(
+          "My Quiz \n Test your skills",
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.white,
+            fontFamily: "Quando",
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
